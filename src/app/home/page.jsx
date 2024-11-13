1d4145f31cbc53dd2314e5ddf66051d19fb2fc8f
odd:odd <eehd80yul@gmail.com>
2024-11-13 11:42:12 +0900

    zz

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -13,14 +13,19 @@ const HomePage = () => {
     >
       <Header />
       {/* ministate */}
-      <main className='inner'>
-        <div className='lg:flex'>
+      <main className='inner flex flex-col gap-10'>
+        <div className='flex flex-col lg:flex-row gap-4'>
           <div className='bg-red-100 grow'>d</div>
-          <div className='bg-blue-100 grow'>d</div>
+          <div className='grow flex flex-col gap-4'>
+            <div className='bg-green-100 grow'>g</div>
+            <div className='bg-green-100 grow'>g</div>
+          </div>
         </div>
 
-        <Button>확인</Button>
-        <Button gray>취소</Button>
+        <div>
+          <Button>확인</Button>
+          <Button gray>취소</Button>
+        </div>
 
         <MiniStateGrid stateData={stateData} />
       </main>
