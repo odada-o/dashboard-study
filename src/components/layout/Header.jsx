bdeb7a56979be46b669c589dec19262dd61757ae
odd:odd <eehd80yul@gmail.com>
2024-11-15 12:16:03 +0900

    navigation

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,9 +4,26 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 
-const Header = () => {
+const navItemsData = [
+  { href: '/about', label: 'about' },
+  { href: '/state', label: 'state' },
+  { href: '/login', label: 'login' },
+]
 
+const NavLink = ({ children, href }) => {
   const pathname = usePathname(); // 현재 경로를 가져옴
+  const isActive = pathname === href;
+
+  return (
+    <Link href={href} 
+            className={`py-2 
+            ${ isActive ? 'text-primary' : ''}`}>
+              { children }
+              </Link>
+  )
+}
+
+const Header = () => {
 
   return (
     <header className='bg-gray-200 p-4 mb-10 flex justify-between items-center'>
@@ -16,17 +33,13 @@ const Header = () => {
       <nav>
         <ul className='flex gap-5'>
           <li>
-            <Link href='/about' 
-            className={`py-2 
-            ${ pathname === '/about' ? 'text-primary' : ''}`}>about</Link>
+            <NavLink href="/about">about</NavLink>
           </li>
           <li>
-            <Link href='/state' className={`py-2 
-            ${ pathname === '/state' ? 'text-primary' : ''}`}>state</Link>
+            <NavLink href="/state">state</NavLink>
           </li>
           <li>
-            <Link href='/login' className={`py-2 
-            ${ pathname === '/login' ? 'text-primary' : ''}`}>login</Link>
+            <NavLink href="/login">login</NavLink>
           </li>
         </ul>
       </nav>
