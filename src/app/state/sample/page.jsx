52b3f43b4622d0295dc285fece9c62eeff98fc04
odd:odd <eehd80yul@gmail.com>
2024-11-20 17:47:38 +0900

    zz

--- src/app/state/sample/page.jsx
+++ src/app/state/sample/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import classNames from 'classnames'
 import React, { useState } from 'react'
 import { FaBars } from 'react-icons/fa6'
 import { IoClose } from 'react-icons/io5'
@@ -10,13 +11,51 @@ const SamplePage = () => {
 
   const [isOpen, setIsOpen] = useState(false)
 
+  const [text, setText] = useState("")
+
+  const sortArr = [
+    {value: "메뉴1"},
+    {value: "메뉴2"},
+    {value: "메뉴3"}
+    ]
+
+    const [tab, setTab] = useState(0)
+
   return (
     <div>
+      <div>
+        {/* tab */}
+        <div>
+          {sortArr.map((item, index) => (
+            <button key={index} onClick={() => {setTab(index)}} className={
+              classNames(tab === index ? 'bg-blue-200' : '')
+            }>{item.value}</button>
+          ))}
+        </div>
+        {/* tab-cont */}
+        <div>
+          {tab === 0 && <div>메뉴1 컨텐츠</div>}
+          {tab === 1 && <div>메뉴2 컨텐츠</div>}
+          {tab === 2 && <div>메뉴3 컨텐츠</div>}
+        </div>
+
+      </div>
+
+
+      <input type="text" value={text}
+        onChange={(e) => {setText(e.target.value)}}
+      className='bg-blue-300 mx-auto my-3 block' /> <p>{text}</p>
+
+      <hr />
+
       <button onClick={() => {setIsOpen(!isOpen)}} className='relative z-10'>
         {isOpen ? <IoClose /> : <FaBars />}
       </button>
-      <div className={`fixed top-0 w-full h-full bg-blue-300 
-        ${isOpen ? 'left-0' : 'left-full'}`}>전체메뉴</div>
+      <div className={`fixed top-0 w-full h-full bg-blue-300 transition-all duration-300 z-0  ${isOpen ? 'left-0' : 'left-full'}`}>전체메뉴
+        </div>
+
+        <div className={classNames('fixed top-0 w-full h-full bg-blue-300 transition-all duration-300 z-0', isOpen ? 'left-0' : 'left-full')}>전체메뉴
+        </div>
 
       <div>{num}</div>
       <button onClick={() => {setNum(num + 1)}}>+</button>
