b5e35ea130675703eff61cba423d4b22753bf38c
odd:odd <eehd80yul@gmail.com>
2024-11-22 17:34:34 +0900

    SS

--- src/app/reducer/page.jsx
+++ src/app/reducer/page.jsx
@@ -1,41 +1,25 @@
 "use client"
+import { ACTION_TYPE, setCount } from '@/reducers/counterReducer'
 import React, { useReducer, useState } from 'react'
-
 const buttonStyle = 'px-4 py-2 bg-blue-500 text-white rounded'
 
-// count를 변경해서 다시 보내주는 함수
-const setCount = (state, action) => {
-  // const action = {type: 'INCREMENT'}
-  // switch문으로 action.type에 따라 다른 동작을 수행
-  switch (action.type) {
-    case 'INCREMENT':
-      return {counter: state.counter + 1}
-    case 'DECREMENT':
-      return {counter: state.counter - 1}
-    case 'RESET':
-      return {counter: 0}
-    default:
-      return state
-  }
-}
-
 const ReducerPage = () => {
-  // const [count, setCount] = useState(0)
-
   // const [현재상태, 파견함수] = useReducer(상태변경함수, 초기값)
   const [count, dispatch] = useReducer(setCount, {
     counter: 0,
+    name: 'dd',
   })
+  // const count = {counter: 0, name: 'dd'}
 
   return (
     <div className='flex justify-center gap-4 py-5 text-4xl'>
         <button 
-          onClick={() => {dispatch({type: 'DECREMENT'})}}>-</button>
-        <strong>{count.counter}</strong>
+          onClick={() => {dispatch({type: ACTION_TYPE.DECREMENT})}}>-</button>
+        <strong>{count.name} : {count.counter}</strong>
         <button 
-          onClick={() => {dispatch({type: 'INCREMENT'})}}>+</button>
+          onClick={() => {dispatch({type: ACTION_TYPE.INCREMENT})}}>+</button>
 
-          <button onClick={() => {dispatch({type: 'RESET'})}}>reset</button>
+          <button onClick={() => {dispatch({type: ACTION_TYPE.RESET})}}>reset</button>
     </div>
   )
 }
